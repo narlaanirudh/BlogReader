c3ca889c9d244e53bc64ee1e2598c72d53d1d603
narlaanirudh <narlaanirudh@gmail.com>
2013-11-04 15:36:48 +0000

    Initial Commit

---
+++ BlogReaderS/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  BlogReaderS
+//
+//  Created by Anirudh narla on 11/4/13.
+//  Copyright (c) 2013 Anirudh Narla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
